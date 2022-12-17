c0011ae24b0aae29632fea9936c08ebe3e2679ca
M4ttDev <wip.zimny@gmail.com>
2022-12-17 10:45:01 +0100

    new changes

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Script from 'next/script'
 
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import React from 'react';
+
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
+
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 const name = 'M4ttDev'
 export const siteTitle = 'Scheduline'
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,19 +8,13 @@ export default function FirstPost() {
   return (
     <>
         <Layout>
-          <div className="w-80 bg-white shadow rounded">
-            <h1 className='text-lg font-medium'>First Post</h1>
-          </div>
-            
+            <h1>First Post</h1>
             <Image
                 src="/images/profile.jpg" // Route of the image file
                 height={144} // Desired size with correct aspect ratio
                 width={144} // Desired size with correct aspect ratio
                 alt="Your Name"
             />
-            <h2>
-            <Link href="/">Back to home</Link>
-            </h2>
         </Layout>
     </>
   );
